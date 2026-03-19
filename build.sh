11e396d7a3428722c535e58604aa8b4d116b5795
john <john@gmail.com>
2026-03-19 13:37:15 +0100

    DFD

--- build.sh
+++ build.sh
@@ -4,3 +4,17 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Create admin user if it doesn't exist
+python manage.py shell -c "
+from accounts.models import User
+if not User.objects.filter(email='admin@apc.com').exists():
+    u = User.objects.create_user(email='admin@apc.com', password='admin123', first_name='Admin', last_name='User')
+    u.role = 'admin'
+    u.is_staff = True
+    u.is_superuser = True
+    u.save()
+    print('Admin user created.')
+else:
+    print('Admin user already exists.')
+"
